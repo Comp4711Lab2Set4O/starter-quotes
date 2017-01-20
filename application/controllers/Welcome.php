7cff95b8d2c4a747daaafdd3a7665524c4514adf
jacobmcphail <jacobmcphail@gmail.com>
2017-01-20 11:46:39 -0800

    Add Random

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -27,6 +27,28 @@ class Welcome extends Application
 		}
 		$this->data['authors'] = $authors;
 
+		$this->random();
+	}
+
+	public function random()
+	{
+		// this is the view we want shown
+		$this->data['pagebody'] = 'homepage';
+
+		// build the list of authors, to pass on to our view
+		$source = $this->quotes->all();
+		$authors = array ();
+
+		$rand_keys = array_rand($source, 1);
+		$record = $source[$rand_keys];
+		$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where'], 'what' => $record['what']);
+
+		/*foreach ($source as $record)
+		{
+			$authors[] = array ('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where'], 'what' => $record['what']);
+		} */
+		$this->data['authors'] = $authors;
+
 		$this->render();
 	}
 
